eb935272a937a5a2c87721824f82328965a1507d
Denielle Abaquita <denielle.abaquita@gmail.com>
2021-10-09 14:22:53 -0400

    Added all needed view controllers

---
+++ BetterKnow/Views/Home/TopicSheetViewController.swift
@@ -0,0 +1,28 @@
+//
+//  TopicSheetViewController.swift
+//  BetterKnow
+//
+//  Created by Denielle Abaquita on 10/9/21.
+//
+
+import UIKit
+
+class TopicSheetViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        modalPresentationStyle = .formSheet
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
