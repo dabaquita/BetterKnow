b4df2b97e95187c68c9e0ae36d2b377d18bacea3
Denielle Abaquita <denielle.abaquita@gmail.com>
2021-10-09 12:39:35 -0400

    Added required init

--- BetterKnow/TableView/Info/InfoTableView.swift
+++ BetterKnow/TableView/Info/InfoTableView.swift
@@ -12,5 +12,9 @@ class InfoTableView: UITableView {
     override init(frame: CGRect, style: UITableView.Style) {
         super.init(frame: frame, style: style)
     }
-
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
 }
