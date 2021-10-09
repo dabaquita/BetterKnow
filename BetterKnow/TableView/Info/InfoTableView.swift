ea5b2d1c49b50a1b57eb872555c35eb6b07b988c
Denielle Abaquita <denielle.abaquita@gmail.com>
2021-10-09 12:39:11 -0400

    Replaced tableviewVC with tableview

---
+++ BetterKnow/TableView/Info/InfoTableView.swift
@@ -0,0 +1,16 @@
+//
+//  InfoTableView.swift
+//  BetterKnow
+//
+//  Created by Denielle Abaquita on 10/9/21.
+//
+
+import UIKit
+
+class InfoTableView: UITableView {
+
+    override init(frame: CGRect, style: UITableView.Style) {
+        super.init(frame: frame, style: style)
+    }
+
+}
