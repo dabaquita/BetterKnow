40081d29834e6a227acc1c1112fdd6ee338d5c2e
Denielle Abaquita <denielle.abaquita@gmail.com>
2021-10-10 09:06:05 -0400

    Added new colors

--- BetterKnow/Constants/Colors.swift
+++ BetterKnow/Constants/Colors.swift
@@ -13,4 +13,12 @@ public enum Colors {
     static let yellow = UIColor(named: "Yellow")
     static let lightOrange = UIColor(named: "LightOrange")
     static let orange = UIColor(named: "Orange")
+    static let peach = UIColor(named: "Peach")
+    static let offWhite = UIColor(named: "OffWhite")
+    static let offWhite2 = UIColor(named: "OffWhite2")
+    static let vanilla = UIColor(named: "Vanilla")
+    static let vanilla2 = UIColor(named: "Vanilla2")
+    static let lightVanilla = UIColor(named: "LightVanilla")
+    static let babyBlue = UIColor(named: "BabyBlue")
+    static let gray = UIColor(named: "Gray")
 }
