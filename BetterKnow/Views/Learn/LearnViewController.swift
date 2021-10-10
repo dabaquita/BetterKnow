652c0217d6b7622457c967615e474035ea3f3837
Denielle Abaquita <denielle.abaquita@gmail.com>
2021-10-09 23:44:41 -0400

    Added tableview similar to HomeVC

--- BetterKnow/Views/Learn/LearnViewController.swift
+++ BetterKnow/Views/Learn/LearnViewController.swift
@@ -8,11 +8,113 @@
 import UIKit
 
 class LearnViewController: UIViewController {
+    
+    let tableView = UITableView()
+    
+    // let quizzes = Quiz.FetchQuizzes()
+    // let featured = Feature.FetchFeatured()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
-        navigationItem.title = "BetterKnow"
+        navigationItem.title = "Learn"
+        
+        configureTableView()
     }
+    
+    private func configureTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(InfoCell.self, forCellReuseIdentifier: InfoCell.identifier)
+        tableView.register(InfoTableViewHeaderView.self, forHeaderFooterViewReuseIdentifier: InfoTableViewHeaderView.identifier)
+        view.addSubview(tableView)
+        
+        // Constraints
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
+    
+}
+
+extension LearnViewController: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // Categories section
+        if section == 1 {
+            return 4
+        }
+        // Feature section
+        else {
+            return 2
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return view.bounds.height / 10
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        guard let header = tableView.dequeueReusableHeaderFooterView(
+                withIdentifier: InfoTableViewHeaderView.identifier
+        ) as? InfoTableViewHeaderView else {
+            print("ATTENTION - Could not get InfoTableViewHeaderView")
+            return UITableViewHeaderFooterView()
+        }
+        if section == 1 {
+            header.contentView.backgroundColor = Colors.navy
+            header.title.text = "Quizzes"
+        } else {
+            header.contentView.backgroundColor = Colors.orange
+            header.title.text = "Featured"
+        }
+        return header
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return view.bounds.height / 5
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+        // Present new content here
+        let quizVC = QuizViewController()
+        navigationController?.pushViewController(quizVC, animated: true)
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(
+            withIdentifier: InfoCell.identifier,
+            for: indexPath
+        ) as? InfoCell else {
+            print("ATTENTION - Could not load Info Cell")
+            return UITableViewCell()
+        }
+        
+        // For all cells
+        cell.accessoryType = .disclosureIndicator
+        
+        // Configure Categories section cells
+        if indexPath.section == 1 {
+            cell.backgroundColor = Colors.blue
+            //cell.textLabel?.text = catData[indexPath.row].title
+        }
+        // Configure Featured section cells
+        else {
+            cell.backgroundColor = Colors.lightOrange
+            //cell.textLabel?.text = featData[indexPath.row].title
+        }
+        
+        return cell
+    }
+    
     
 }
